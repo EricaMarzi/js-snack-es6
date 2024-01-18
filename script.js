eaa6ee4546d568587a3704cfd2f7ddda277bcbe0
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-18 09:52:12 +0100

    snack 3 done

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ const plates = students.map(({name}) => name.toUpperCase())
 // console.log(newBestStudents);
 // console.log(plates);
 
-//& Snack 3 ✘
+//& Snack 3 ✔
 /* 
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso. Stampare in console la bici con peso minore utilizzando il destructuring
